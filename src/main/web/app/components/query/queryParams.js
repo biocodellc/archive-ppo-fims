5191abaa6c53d493bd9380afbcb045d5c2e381b6
rjewing <ewing.rj@gmail.com>
2017-05-20 09:42:15 -0700

    fix traits queryParam

--- src/main/web/app/components/query/queryParams.js
+++ src/main/web/app/components/query/queryParams.js
@@ -35,7 +35,7 @@
             var builder = new QueryBuilder();
 
             angular.forEach(params.traits, function (t) {
-                builder.add("+types:\"" + t + "\"");
+                builder.add("+plantStructurePresenceTypes:\"" + t + "\"");
             });
 
             if (params.fromYear) {
